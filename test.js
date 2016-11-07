d14445b71756268d78d7f7b632d637067c897390
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-11-07 20:35:53 +0530

    spaces >> tabs :metal:

--- test.js
+++ test.js
@@ -3,11 +3,9 @@ var assert = require('assert');
 var npmJanitor = require('./');
 
 it('should return an array of json validator data. ', function (done) {
-	this.timeout(150000);
-	npmJanitor('hemanth', function (err,data) {
-		assert.equal(data.length > 1, true, "Expected some data to be present!");
-		done();
-	});
+    this.timeout(150000);
+    npmJanitor('hemanth', function (err,data) {
+        assert.equal(data.length > 1, true, "Expected some data to be present!");
+        done();
+    });
 });
-
-
