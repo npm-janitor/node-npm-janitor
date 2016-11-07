1b660cc151f8db4646acbcea6fc01af46aa77539
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-11-07 21:00:04 +0530

    Too impatient for 150000

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 var npmJanitor = require('./');
 
 it('should return an array of json validator data. ', function (done) {
-    this.timeout(150000);
+    this.timeout(15000);
     npmJanitor('hemanth', (err, data) => {
         assert.equal(data.length > 1, true, "Expected some data to be present!");
         done();
